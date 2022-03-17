f9776eb6e3ee1dfbcc06149c4e595d8cc9471247
Hashmatullah Nazari <hn0153@gmail.com>
2022-03-17 17:20:59 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+function myFunction1() {
+
+   var output = document.getElementById('output');
+  output.style.backgroundRepeat = "no-repeat";
+output.style.backgroundImage = "url('https://worldgoo.com/wp-content/uploads/2019/05/What-is-the-sea-at-the-beaches-of-Sunny-Beach.jpg')";
+  
+ output.style.width = '100%'; 
+  output.style.height= '700px'; 
+  style.backgroundPosition="center center";
+}
+
+function myFunction2() {
+
+  var output =  document.getElementById('output');
+  output.style.backgroundRepeat = "no-repeat";
+output.style.backgroundImage = "url('https://thumbs.dreamstime.com/b/beautiful-sunny-beach-view-nice-tropical-beach-palms-ar-beautiful-sunny-beach-view-nice-tropical-beach-palms-104749857.jpg')";
+  output.style.width = '100%'; 
+  output.style.height= '700px'; 
+}
+
+function myFunction3() {
+
+  var output =  document.getElementById('output');
+  output.style.backgroundRepeat  = "no-repeat";
+output.style.backgroundImage = "url('https://www.solemiami.com/wp-content/uploads/2020/02/beach-aerial-horz.jpg')";
+  output.style.width = '100%'; 
+  output.style.height= '700px'; 
+}
+
+
